9d3b24b0723b596a9f38869409abbf391e372aa2
David Zhu <david@dzhy.dev>
2020-06-08 03:16:01 -0700

    working on bspwm

--- setup-system.sh
+++ setup-system.sh
@@ -2,6 +2,8 @@
 
 # only tested for pop os 
 
+## vim and terminal setup
+
 # install alacritty
 # sudo add-apt-repository ppa:mmstick76/alacritty # only needed for ubuntu
 sudo apt install -y alacritty
@@ -17,3 +19,12 @@ sudo apt install -y universal-ctags neovim nodejs npm tmux python3-pip
 # coc-python dependencies
 pip3 install --user jedi black
 
+## awesome wm setup
+sudo apt install -y awesome
+
+## bspwm setup
+sudo apt install -y bspwm dmenu feh
+
+sudo snap install --edge --devmode polybar-git
+
+sudo snap alias polybar-git polybar
