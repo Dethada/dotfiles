a8bae87340308e0d060802c964691bee8ff63765
David Zhu <david@dzhy.dev>
2020-06-10 02:27:54 +0800

    tested setup script on manjaro

--- scripts/setup-arch.sh
+++ scripts/setup-arch.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# tested on manjaro
+
 # install packages
-sudo pacman -Sy --noconfirm alacritty neovim universal-ctags tmux python-pip nodejs npm xorg-server xorg-xinit xorg-xrandr lightdm lightdm-gtk-greeter bspwm sxhkd 
+sudo pacman -Sy --noconfirm alacritty neovim universal-ctags tmux python-pip nodejs npm xorg-server xorg-xinit xorg-xrandr lightdm lightdm-gtk-greeter bspwm sxhkd feh firefox dmenu
 
 # install powerline fonts
 git clone https://github.com/powerline/fonts.git --depth=1 /tmp/fonts
@@ -13,20 +15,14 @@ pip3 install --user jedi black
 
 # yabar
 # will have sudo and pacman prompt
-# yay -S --removemake --nodiffmenu --noeditmenu --nouseask --nocleanmenu --noupgrademenu yabar-git
-git clone https://github.com/geommer/yabar /tmp/yabar
-cd /tmp/yabar
-yabar_dependencies="asciidoc docbook-xml"
-sudo pacman -S $yabar_dependencies 
-make yabar
-# dependencies for building documentation
-make doc
-sudo make install
-sudo pacman -Rsn $yabar_dependencies
+yay -S --removemake --nodiffmenu --noeditmenu --nouseask --nocleanmenu --noupgrademenu yabar-git
 
 # lockscreen
 sudo systemctl enable lightdm.service
-sudo systemctl start lightdm.service
+
+# dhcp client
+sudo systemctl enable dhcpcd
+sudo systemctl start dhcpcd
 
 # if in vm install vmware tools
 if grep -qi vmware /sys/class/dmi/id/product_name; then
@@ -36,3 +32,4 @@ if grep -qi vmware /sys/class/dmi/id/product_name; then
   sudo systemctl enable vmware-vmblock-fuse.service
   sudo systemctl start vmware-vmblock-fuse.service
 fi
+sudo systemctl start lightdm.service
