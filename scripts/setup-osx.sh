92e0973cc56caf792de601d988b8e48606efb935
David <david@dzhy.dev>
2022-07-10 14:51:07 +0800

    Added entr

--- scripts/setup-osx.sh
+++ scripts/setup-osx.sh
@@ -9,7 +9,7 @@ softwareupdate --install-rosetta # required for docker
 
 brew tap homebrew/cask-fonts
 brew install node tmux neovim exa bat ripgrep code jq fd wget poetry tree docker-compose ctags go gnupg fzf gh svn \
-             lf poppler navi exiftool highlight youtube-dl openjdk clojure
+             lf poppler navi exiftool highlight youtube-dl openjdk clojure entr
 brew install --cask visual-studio-code iterm2 telegram whatsapp obsidian docker insomnia
 font-hack-nerd-font mactex-no-gui zotero discord zoom whichspace amethyst
 
@@ -28,3 +28,6 @@ nvim --headless +PlugInstall +qall
 
 # cht.sh script
 curl https://cht.sh/:cht.sh -o $HOME/scripts/cht-core.sh
+
+# display full path in finder
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
