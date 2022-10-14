f5e2e3b75cadf7142752ccab92a01b26d7fa05a4
David <david@dzhy.dev>
2022-10-14 10:20:35 +0800

    Added java specific config

--- .config/nvim/lua/plug-config/undotree.lua
+++ .config/nvim/lua/plug-config/undotree.lua
@@ -5,5 +5,5 @@ vim.g['undotree_ShortIndicators'] = 1
 local wk = require('which-key')
 
 wk.register({
-  ["<leader>u"] = { '<Cmd>UndotreeToggle<CR>', 'Undotree Toggle' },
+  ["<space>u"] = { '<Cmd>UndotreeToggle<CR>', 'Undotree Toggle' },
 })
