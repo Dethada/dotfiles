46028305f8dccf7531b3877bc42c0d6f5eaaefa5
David <david@dzhy.dev>
2023-02-08 23:19:53 +0800

    Changed copilot keymap

--- .config/nvim/lua/plug-config/copilot.lua
+++ .config/nvim/lua/plug-config/copilot.lua
@@ -1,2 +1,2 @@
-vim.keymap.set('i', '<C-]>', '<Plug>(copilot-next)')
-vim.keymap.set('i', '<C-[>', '<Plug>(copilot-previous)')
+vim.keymap.set('i', '<S-Tab>', '<Plug>(copilot-next)')
+-- vim.keymap.set('i', '<C-[>', '<Plug>(copilot-previous)')
