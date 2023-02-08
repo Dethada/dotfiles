9452f567a98fa550ba2321da5d56b37c79c16699
David <david@dzhy.dev>
2023-02-08 23:12:40 +0800

    Edited copilot key map

---
+++ .config/nvim/lua/plug-config/copilot.lua
@@ -0,0 +1,2 @@
+vim.keymap.set('i', '<C-]>', '<Plug>(copilot-next)')
+vim.keymap.set('i', '<C-[>', '<Plug>(copilot-previous)')
---
+++ .config/nvim/lua/plug-config/vimtex.lua
@@ -0,0 +1,16 @@
+vim.g['tex_flavor'] = 'latex'
+
+-- Viewer options: One may configure the viewer either by specifying a built-in
+-- viewer method:
+vim.g['vimtex_view_method'] = 'skim'
+
+-- automatic forward focus after compiling
+vim.g['vimtex_view_skim_activate'] = 1
+vim.g['vimtex_view_skim_sync'] = 1
+
+-- set build directory
+vim.g['vimtex_compiler_latexmk'] = { build_dir = 'build'}
+
+-- Most VimTeX mappings rely on localleader and this can be changed with the
+-- following line. The default is usually fine and is the symbol "\".
+vim.cmd('let maplocalleader = ","')
