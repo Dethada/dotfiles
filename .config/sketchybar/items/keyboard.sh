0efe2d9145075cecfc9a92ca2e6ca8052c476614
David <david@dzhy.dev>
2026-01-07 10:22:37 +0800

    remove old stuff

--- .config/sketchybar/items/keyboard.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sketchybar --add       event              input_change 'AppleSelectedInputSourcesChangedNotification' \
-           --add       item               keyboard right                                              \
-           --set       keyboard           script="$PLUGIN_DIR/keyboard.sh"                            \
-                                          width=44                                                    \
-                                          label.font="$FONT:Bold:14.0"                      \
-           --subscribe keyboard           input_change
